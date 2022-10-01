980a21bb808cdf13735ee3649ab0195111e1912b
Thomas Bonenfant <thomasjean.bonenfant@mail.polimi.it>
2022-10-01 16:35:47 +0200

    Changed Symbolic computation into a function and, modified drawConic function and saved variables

--- drawConic.m
+++ drawConic.m
@@ -1,10 +1,14 @@
-function drawConic(C)
-    im = zeros(3000, 3000);
-    for i=1:3000
-        for j=1:3000
+function drawConic(C, image)
+    im_size = size(image);
+    im = zeros(im_size(1), im_size(2));
+    for i=1:im_size(1)
+        for j=1:im_size(2)
             im(i,j)=[j i 1]*C*[j i 1]';
+            if im(i,j) < 0
+                image(i,j) = 1;
+            end
         end
     end
 
-    imshow(im > 0);
+    imshow(image);
 end
